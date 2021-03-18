8d91f0c5e6494c40d0a6b469b4b76abd575d3f28
Monteiro-Rafael <rafaeldonmonteiro@gmail.com>
2021-03-18 14:47:08 -0300

    toLowerCase and sort added

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 let caminho = path.join(__dirname, '..')
 
-const simbolos = ['-', '!', '"', '?', '.', ',']
+
+
+const simbolos = ['-', '!', '"', '?', '.', ',', '♪', ':', '[', ']', '$', "'"]
 
 funcoes.lerDiretorio(caminho)
     .then(arquivo => arquivo.filter(item => item.includes('.srt'))) // Seleciona todos os arquivos SRT da pasta
@@ -11,10 +13,17 @@ funcoes.lerDiretorio(caminho)
     .then(conteudos => conteudos.join('')) 
     .then(todoConteudo => todoConteudo.split('\n'))
     .then(linhas => linhas.filter(texto => !texto.includes('\r'))) // tira todos os \r 
-    .then(texto => texto.filter(linha => !linha.includes('-->'))) // tira todos os elementos que contem -->    
+    .then(texto => texto.filter(linha => !linha.includes('-->' || '[' || ']'))) // tira todos os elementos que contem -->    
     .then(texto => texto.filter(el => el.trim()))
     .then(texto => texto.filter(el => parseInt(el)!==parseInt(el))) // tira todas as linhas que contem apenas numeros (NaN !== NaN)
     .then(funcoes.removerSimbolos(simbolos)) 
     .then(texto => texto.map(el => el.trim())) // tira todos os espaços
+    .then(separaPalavras => separaPalavras.map(el => el.split(' ')).join())
+    .then(geraArray => geraArray.split(','))
+    .then(texto => texto.filter(el => parseInt(el)!==parseInt(el))) // tira todas as linhas que contem apenas numeros (NaN !== NaN)
+    .then(array => array.map(item => item.toLowerCase()))
+    .then(array => array.sort())
+    .then(array => array.filter(item => item.trim()))
     .then(console.log)
+    
 
